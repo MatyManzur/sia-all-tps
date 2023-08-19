58203371866003691c78be23b8a9509e3950921b
Juan Ramiro Castro <juancastro@itba.edu.ar>
2023-08-19 16:54:05 -0300

    Added global greedy

--- sia-tp1/src/algorithms.py
+++ sia-tp1/src/algorithms.py
@@ -14,6 +14,7 @@ class BFSAlgorithm(Algorithm):
     def _add_to_frontier(self, new_node: Node):
         self.frontier.appendleft(new_node)
 
+
 class DFSAlgorithm(Algorithm):
 
     def __init__(self, board: Board, player_position: Position, box_positions: List[Position]):
@@ -45,6 +46,35 @@ class AStarAlgorithm(Algorithm):
     def __iter__(self):
         aux = Node(self.initial_state, 0, None)
         self.frontier.put((aux.score, aux))
+        return self
+
+    def get_solution_info(self) -> SolutionInfo:
+        if not self.has_solution():
+            raise 'A solution must be found to return info!'
+        return SolutionInfo(self.solution.get_path_from_root(), self.solution.cost, len(self.visited.keys()),
+                            self.frontier.qsize())
+
+
+class GlobalGreedyAlgorithm(Algorithm):
+
+    def __init__(self, board: Board, player_position: Position, box_positions: List[Position],
+                 heuristic: Callable[[Position, List[Position], Board], int]):
+        super().__init__(board, player_position, box_positions, heuristic)
+        self.frontier = queue.PriorityQueue()
+
+    @staticmethod
+    def _visited_value(node: Node) -> int:
+        return node.score
+
+    def _add_to_frontier(self, new_node: Node):
+        self.frontier.put((new_node.state.heuristic_value, new_node))
+
+    def _get_item_from_frontier(self) -> Node:
+        return self.frontier.get()[1]
+
+    def __iter__(self):
+        aux = Node(self.initial_state, 0, None)
+        self.frontier.put((aux.state.heuristic_value, aux))
         return self
 
     def get_solution_info(self) -> SolutionInfo:
