fix: backpropagation calculus fuck calculus

sia-tp3/src/layer.py
@@ -10,6 +10,7 @@ class Layer:  # N neuronas, con M inputs
         self.activation_function = activation_function
         self.output = None      # Aca se guarda el resultado despues de salir de la funcion de activacion
         self.excitement = None  # Aca se guarda el valor de la suma ponderada del ultimo input
+        self.pending_weight = None
 
     #
     def forward(self, inputs):  # inputs[0] must be 1 for bias
@@ -19,11 +20,17 @@ class Layer:  # N neuronas, con M inputs
     # NxM * Mx1 = Nx1
     def get_excitement(self, inputs=None):
         if inputs is not None:
-            self.excitement = np.dot(self.weights, inputs)
+            self.excitement = np.matmul(self.weights, inputs)
         return self.excitement
     
-    def add_weights(self, delta: NDArray):
-        self.weights = np.sum(self.weights, delta)
+    def set_pending_weights(self, delta: NDArray):
+        self.weights = delta
+
+    def consolidate_weights(self):
+        self.weights = np.sum(self.weights, self.pending_weight)
+
+    def reset_weights(self):
+        self.weights = np.subtract(self.weights, self.pending_weight)
 
 def generate_layers(layer_neurons: List[int], initial_inputs: int, act_func: Activation_Function) -> List[Layer]:
     prev_value = initial_inputs
@@ -33,7 +40,7 @@ def generate_layers(layer_neurons: List[int], initial_inputs: int, act_func: Act
         prev_value = neuron_count
     return generate_layers
 
-def train_excitement(layer_neurons: List[Layer], training_data: NDArray) -> List[Layer]:
+def forward_propagation(layer_neurons: List[Layer], training_data: NDArray) -> List[Layer]:
     input = training_data
     for layer in layer_neurons:
         input = layer.forward(input)
@@ -42,16 +49,17 @@ def train_excitement(layer_neurons: List[Layer], training_data: NDArray) -> List
 def backpropagation(layer_neurons: List[Layer], derivative_func: Activation_Function, expected_output: NDArray, input: NDArray, learning_bias: float) -> List[Layer]:
     # Nx1 * Nx1 = Nx1
     initial_gradient = np.multiply(expected_output - layer_neurons[-1].output, derivative_func(layer_neurons[-1].excitement))
-    # Resultado: NxM, Nx1.1xM
-    initial_delta = learning_bias * np.dot(initial_gradient, np.transpose(layer_neurons[-2].output))
-    layer_neurons[-1].add_weights(initial_delta)
+    # Resultado: Nx1*1xM = NxM
+    initial_delta = learning_bias * np.matmul(initial_gradient, np.transpose(layer_neurons[-2].output))
+    layer_neurons[-1].set_pending_weights(initial_delta)
     previous_gradient = initial_gradient
     previous_layer = layer_neurons[-1]
     # reverse iteration
     for layer, i in layer_neurons[0:-1:-1]:
-        # MxN.Nx1 = Mx1 => Nx1
-        gradient = derivative_func(layer.excitement) * np.dot(np.transpose(previous_layer.weights), previous_gradient)
-        prev_input = layer[i - 1].output if i != 0 else input
-        delta = learning_bias * np.dot(gradient, np.transpose(prev_input))
-        layer.add_weights(delta)
+        # MxN.Nx1 = Mx1 => Nx1*Nx1 = Nx1
+        gradient = np.multiply(derivative_func(layer.excitement), np.matmul(np.transpose(previous_layer.weights), previous_gradient))  
+        curr_input = layer[i - 1].output if i != 0 else input
+        # Nx1.1xM = NxM
+        delta = learning_bias * (gradient * np.transpose(curr_input))
+        layer.set_pending_weights(delta)
     return layer_neurons