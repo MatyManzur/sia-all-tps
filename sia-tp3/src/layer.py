b4db3945f0844c9441bfa7a3191c44a93f8948a2
franco <francorupnik@gmail.com>
2023-09-20 22:45:45 -0300

    Tp3: terminando ej2

--- sia-tp3/src/layer.py
+++ sia-tp3/src/layer.py
@@ -1,14 +1,19 @@
 import numpy as np
 from functions import Activation_Function
 
-class Layer: #N neuronas, con M inputs
+
+class Layer:  # N neuronas, con M inputs
 
     def __init__(self, num_inputs: int, num_neurons: int, activation_function: Activation_Function):
         self.weights = np.random.rand(num_neurons, num_inputs + 1)
         self.activation_function = activation_function
+        self.excitement = None  # Aca se guarda el valor de la suma ponderada del ultimo input
 
-    def forward(self, inputs): # inputs[0] must be 1 for bias
-        return self.activation_function(np.dot(self.weights, inputs))
+    def forward(self, inputs):  # inputs[0] must be 1 for bias
+        self.excitement = np.dot(self.weights, inputs)
+        return self.activation_function(self.excitement)
 
-    def get_excitement(self,inputs):
-        return np.dot(self.weights, inputs)
+    def get_excitement(self, inputs=None):
+        if inputs is not None:
+            self.excitement = np.dot(self.weights, inputs)
+        return self.excitement
