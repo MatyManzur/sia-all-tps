c851b51aa3c7660aa52d003c3a38996f0c5f48c1
MatyManzur <mmanzur@itba.edu.ar>
2023-09-21 21:05:45 -0300

    Changes by franc

--- sia-tp3/src/layer.py
+++ sia-tp3/src/layer.py
@@ -8,7 +8,7 @@ class Layer:  # N neuronas, con M inputs
     def __init__(self, num_inputs: int, num_neurons: int, activation_function: Activation_Function):
         self.weights = np.random.rand(num_neurons, num_inputs + 1)
         self.activation_function = activation_function
-        self.output = None      # Aca se guarda el resultado despues de salir de la funcion de activacion
+        self.output = None  # Aca se guarda el resultado despues de salir de la funcion de activacion
         self.excitement = None  # Aca se guarda el valor de la suma ponderada del ultimo input
         self.pending_weight = None
 
@@ -27,7 +27,7 @@ class Layer:  # N neuronas, con M inputs
         self.weights = delta
 
     def consolidate_weights(self):
-        self.weights = np.sum(self.weights, self.pending_weight)
+        self.weights += self.pending_weight
 
     def reset_weights(self):
         self.weights = np.subtract(self.weights, self.pending_weight)
@@ -38,7 +38,8 @@ def generate_layers(layer_neurons: List[int], initial_inputs: int, act_func: Act
     for neuron_count in layer_neurons:
         neural_network.append(Layer(prev_value, neuron_count, act_func))
         prev_value = neuron_count
-    return generate_layers
+    return neural_network
+
 
 def forward_propagation(layer_neurons: List[Layer], training_data: NDArray) -> List[Layer]:
     input = training_data
@@ -55,11 +56,28 @@ def backpropagation(layer_neurons: List[Layer], derivative_func: Activation_Func
     previous_gradient = initial_gradient
     previous_layer = layer_neurons[-1]
     # reverse iteration
-    for layer, i in layer_neurons[0:-1:-1]:
+    for i in range(len(layer_neurons) - 2, -1, -1):
         # MxN.Nx1 = Mx1 => Nx1*Nx1 = Nx1
-        gradient = np.multiply(derivative_func(layer.excitement), np.matmul(np.transpose(previous_layer.weights), previous_gradient))  
-        curr_input = layer[i - 1].output if i != 0 else input
+        # δ^m = θ'(h) * ((W^m+1)' * δ^m+1)
+        delta = np.multiply(derivative_func(layer_neurons[i].excitement),
+                            np.matmul((np.transpose(previous_weight_change.weights))[1:], previous_delta))
+        curr_input = layer_neurons[i - 1].output if i != 0 else input
         # Nx1.1xM = NxM
         delta = learning_bias * (gradient * np.transpose(curr_input))
         layer.set_pending_weights(delta)
     return layer_neurons
+
+
+def calculate_error(calculated_output: NDArray, expected_out: NDArray) -> float:
+    return (1 / 2) * (np.sum(np.square(np.subtract(expected_out, calculated_output))))
+
+
+
+def consolidate_weights(neural_net: List[Layer]):
+    for layer in neural_net:
+        layer.consolidate_weights()
+
+
+def reset_pending_weights(neural_net: List[Layer]):
+    for layer in neural_net:
+        layer.reset_pending_weights()
