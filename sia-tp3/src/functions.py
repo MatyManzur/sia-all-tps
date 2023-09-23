05e0d79377f589712f0fc2da2f4d06b1081fe7ef
Juan Ramiro Castro <juancastro@itba.edu.ar>
2023-09-23 19:00:33 -0300

    Improve performance

--- sia-tp3/src/functions.py
+++ sia-tp3/src/functions.py
@@ -9,11 +9,13 @@ BETA = 1
 
 ##LAS FUNCIONES DEBERIAN RECIBIR UN VECTOR COLUMNA CON LA SUMA DE LOS Wi*Xi
 
+
 def identity(x: NDArray | float) -> NDArray:
     return x
 
+
 def derivative_identity(x: NDArray | float) -> NDArray:
-    return np.apply_along_axis(lambda h: h-h+1, 0, x)  # no preguntes
+    return np.ones_like(x)
 
 
 def identity_normalization(x: NDArray | float) -> NDArray | float:
@@ -21,43 +23,35 @@ def identity_normalization(x: NDArray | float) -> NDArray | float:
 
 
 def sign(x: NDArray | float) -> NDArray | float:
-    return np.apply_along_axis(inclusive_sign, 0, x)
+    return -1 + 2 * (x >= 0)
 
 
 def inclusive_sign(x) -> int:
-    return -1 + 2*(x >= 0)
+    return -1 + 2 * (x >= 0)
 
 
 def hiperbolic(x: NDArray | float) -> NDArray | float:
-    if isinstance(x, float) or isinstance(x, int):
-        return BETA * np.tanh(x)
-    return np.apply_along_axis(lambda h: BETA * np.tanh(h), 0, x)
+    return BETA * np.tanh(x)
 
 
 def hiperbolic_derivative(x: NDArray | float) -> NDArray | float:
-    if isinstance(x, float) or isinstance(x, int):
-        return BETA * (1 - (BETA * np.tanh(x)) ** 2)
-    return np.apply_along_axis(lambda h: BETA * (1 - (BETA * np.tanh(h)) ** 2), 0, x)
+    return BETA * (1 - (BETA * np.tanh(x)) ** 2)
 
 
 def hiperbolic_normalization(x: NDArray | float) -> NDArray | float:
     if isinstance(x, float) or isinstance(x, int):
         return x / abs(x)
     max_value = max(abs(x))
-    return np.apply_along_axis(lambda h: h/max_value, 0, x)
+    return x / max_value
 
 
 def sigmoid(x: NDArray) -> NDArray:
-    if isinstance(x, float) or isinstance(x, int):
-        return 1 / (1 + np.exp(-2 * BETA * x))
-    return np.apply_along_axis(lambda h: 1 / (1 + np.exp(-2 * BETA * h)), 0, x)
+    return 1 / (1 + np.exp(-2 * BETA * x))
 
 
 def sigmoid_derivative(x: NDArray | float) -> NDArray | float:
-    if isinstance(x, float) or isinstance(x, int):
-        return 2 * BETA * (1 / (1 + np.exp(-2 * BETA * x))) * (1 - (1 / (1 + np.exp(-2 * BETA * x))))
-    return np.apply_along_axis(
-        lambda h: 2 * BETA * (1 / (1 + np.exp(-2 * BETA * h))) * (1 - (1 / (1 + np.exp(-2 * BETA * h)))), 0, x)
+    return (2 * BETA * np.exp(-2 * BETA * x))/(1 + np.exp(-2 * BETA * x)) **2
+
 
 
 def sigmoid_normalization(x: NDArray | float) -> NDArray | float:
@@ -65,4 +59,4 @@ def sigmoid_normalization(x: NDArray | float) -> NDArray | float:
         return x / x
     max_value = max(x)
     min_value = min(x)
-    return np.apply_along_axis(lambda h: (h - min_value) / (max_value - min_value), 0, x)
+    return (x - min_value) / (max_value - min_value)
