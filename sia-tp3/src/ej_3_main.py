Fix everything I did wrong on normalization like a champ

sia-tp3/src/ej_3_main.py
@@ -11,21 +11,20 @@ from data.ej3_digitos_par import DATA_DIGITOS_PAR
 
 # Un entrenamiento del neural net
 def train_perceptron(neural_net: List[Layer], learning_constant: float, training_item: Tuple[NDArray, NDArray],
-                     activation_func: Activation_Function, derivative_fun: Activation_Function,
-                     normalization_func: Activation_Function):
+                     activation_func: Activation_Function, derivative_fun: Activation_Function):
     inputs, expected = training_item
     forward_propagation(neural_net, inputs)
-    backpropagation(neural_net, derivative_fun, normalization_func(expected), inputs, learning_constant)
+    backpropagation(neural_net, derivative_fun, expected, inputs, learning_constant)
 
     return neural_net
 
 
-def calculate_error_from_items(neural_net: List[Layer], items: List[Tuple[Tuple, Tuple]], output_func) -> float:
+def calculate_error_from_items(neural_net: List[Layer], items: List[Tuple[Tuple, Tuple]]) -> float:
     error_sum = 0
     for inputs, expected in items:
         sample, expected_output = np.array(inputs), np.array(expected)
         outputs = np.array(forward_propagation(neural_net, sample))
-        expected_output = output_func(np.reshape(expected_output, outputs.shape))
+        expected_output = np.reshape(expected_output, outputs.shape)
         error_sum += calculate_error(outputs, expected_output)
     return error_sum
 
@@ -70,16 +69,18 @@ CONSTANT_RATE_EPS = 0.0001
 
 
 def multilayer_perceptron(layers_neuron_count: List[int], act_func: Activation_Function,
-                          deriv_func: Activation_Function,
-                          output_func,
-                          data: List[Tuple[Tuple, Tuple]]):
+                          deriv_func: Activation_Function, output_func, data: List[Tuple[Tuple, Tuple]]):
     # La capa final tiene tantos nodos como outputs
     layers_neuron_count.append(len(data[0][1]))
     optimization = {}
     optimization['type'] ='momentum'
     optimization['beta'] = BETA
 
-    network = generate_layers(layers_neuron_count, len(data[0][0]), act_func, optimization)
+    unormalized_results = np.array(map(lambda x: x[1], data))
+    normalized_results = output_func(unormalized_results)
+    normalized_data = list(map(lambda x: (x[1][0], normalized_results[x[0]]), enumerate(data)))
+
+    network = generate_layers(layers_neuron_count, len(normalized_data[0][0]), act_func, optimization)
     min_err = float('inf')
     w_min = None
     i = 0
@@ -93,7 +94,7 @@ def multilayer_perceptron(layers_neuron_count: List[int], act_func: Activation_F
     elif ALGORITHM == 'mini-batch':
         sample_size = MINI_BATCH_SIZE
     elif ALGORITHM == 'batch':
-        sample_size = len(data)
+        sample_size = len(normalized_data)
     else:
         raise Exception('Invalid Algorithm!')
 
@@ -101,15 +102,15 @@ def multilayer_perceptron(layers_neuron_count: List[int], act_func: Activation_F
 
     while i < LIMIT and min_err > EPSILON:
         
-        samples = random.sample(data, sample_size)
+        samples = random.sample(normalized_data, sample_size)
 
         for sample in samples:
             _sample = (np.array(sample[0]), np.array(sample[1]))
-            train_perceptron(network, learning_rate, _sample, act_func, deriv_func, output_func)
+            train_perceptron(network, learning_rate, _sample, act_func, deriv_func)
         consolidate_weights(network)
         reset_pending_weights(network)
 
-        err = calculate_error_from_items(network, data, output_func)
+        err = calculate_error_from_items(network, normalized_data)
         if i < LEARNING_RATE_CHANGE_ITER:
             last_errors.append(err)
         else:

sia-tp3/src/ej_3_test_runner.py
@@ -66,12 +66,12 @@ def class_metrics_info(test_data, network):
     return values_of_classes
 
 
-def complete_confusion_matrix(test_data, network, output_func, confusion_matrix):
+def complete_confusion_matrix(test_data, network, confusion_matrix):
     DETERMINADO_EPSILON = 0.2
     for inputs, expected in test_data:
         values_to_run, expected_output = np.array(inputs), np.array(expected)
         outputs = np.array(forward_propagation(network, values_to_run))
-        expected_output = output_func(np.reshape(expected_output, outputs.shape))
+        expected_output = np.reshape(expected_output, outputs.shape)
         expected_output_index = expected_output.argmax() # Este es el valor de la fila de la matriz
         real_output_index = outputs.argmax() # Ojo, en realidad con tomar el máximo, según Eugenia no alcanza
                                              # Porque podrías tener un 40% como máximo
@@ -79,7 +79,7 @@ def complete_confusion_matrix(test_data, network, output_func, confusion_matrix)
 
 
 def multilayer_perceptron(layers_neuron_count: List[int], act_func: Activation_Function,
-                          deriv_func: Activation_Function, output_func, learning_data: List[Tuple[Tuple, Tuple]],
+                          deriv_func: Activation_Function, learning_data: List[Tuple[Tuple, Tuple]],
                           test_data: List[Tuple[Tuple, Tuple]], epsilon, limit, learning_constant, algorithm,
                           mini_batch_size, optimization_config=None):
     if optimization_config is None:
@@ -107,12 +107,12 @@ def multilayer_perceptron(layers_neuron_count: List[int], act_func: Activation_F
 
         for sample in samples:
             _sample = (np.array(sample[0]), np.array(sample[1]))
-            train_perceptron(network, learning_constant, _sample, act_func, deriv_func, output_func)
+            train_perceptron(network, learning_constant, _sample, act_func, deriv_func)
         consolidate_weights(network)
         reset_pending_weights(network)
 
-        err = calculate_error_from_items(network, learning_data, output_func)
-        test_error = calculate_error_from_items(network, test_data, output_func)
+        err = calculate_error_from_items(network, learning_data)
+        test_error = calculate_error_from_items(network, test_data)
         
         class_metrics_test = class_metrics_info(test_data, network)
         class_metrics_training = class_metrics_info(learning_data, network)
@@ -160,24 +160,31 @@ def run_test(config):
     else:
         raise Exception('Invalid function type!')
 
+
     if config['data_set'] == 'DATA_DIGITOS':
-        learning_data = [x for i, x in enumerate(DATA_DIGITOS) if i in config['learning_data_indexes']]
-        test_data = [x for i, x in enumerate(DATA_DIGITOS) if i in config['test_data_indexes']]
+        data_set = DATA_DIGITOS
     elif config['data_set'] == 'DATA_DIGITOS_PAR':
-        learning_data = [x for i, x in enumerate(DATA_DIGITOS_PAR) if i in config['learning_data_indexes']]
-        test_data = [x for i, x in enumerate(DATA_DIGITOS_PAR) if i in config['test_data_indexes']]
+        data_set = DATA_DIGITOS_PAR
     elif config['data_set'] == 'XOR_DATA':
-        learning_data = [x for i, x in enumerate(XOR_DATA) if i in config['learning_data_indexes']]
-        test_data = [x for i, x in enumerate(XOR_DATA) if i in config['test_data_indexes']]
+        data_set = XOR_DATA
     else:
         raise Exception('Invalid data set!')
+
+    unnormalized_results = np.array(list(map(lambda x: x[1], data_set)))
+    print(unnormalized_results)
+    normalized_results = normalization_function(unnormalized_results)
+    print(normalized_results)
+    normalized_data = list(map(lambda x: (x[1][0], normalized_results[x[0]]), enumerate(data_set)))
+
+    learning_data = [x for i, x in enumerate(normalized_data) if i in config['learning_data_indexes']]
+    test_data = [x for i, x in enumerate(normalized_data) if i in config['test_data_indexes']]
 
     epsilon = config['min_error']
     limit = config['max_iterations']
     learning_constant = config['learning_constant']
     algorithm = config['test']['training_type']
     mini_batch_size = config['test']['mini_batch_size']
-    test_data = multilayer_perceptron(layers, activation_function, derivative_function, normalization_function,
+    test_data = multilayer_perceptron(layers, activation_function, derivative_function,
                                       learning_data, test_data, epsilon, limit, learning_constant, algorithm,
                                       mini_batch_size, config['optimization'])
 

sia-tp3/src/functions.py
@@ -31,9 +31,8 @@ def sign_derivative(x: NDArray | float) -> NDArray | float:
 def sign_normalization(x: NDArray | float) -> NDArray | float:
     if isinstance(x, float) or isinstance(x, int):
         return x / abs(x)
-    max_value = max(abs(x))
-    return x / max_value
-
+    avg = np.average(x)
+    return np.sign(x - avg)
 
 def inclusive_sign(x) -> int:
     return -1 + 2 * (x >= 0)
@@ -50,8 +49,9 @@ def hiperbolic_derivative(x: NDArray | float) -> NDArray | float:
 def hiperbolic_normalization(x: NDArray | float) -> NDArray | float:
     if isinstance(x, float) or isinstance(x, int):
         return x / abs(x)
-    max_value = max(abs(x))
-    return x / max_value
+    max_value = np.max(x)
+    min_value = np.min(x)
+    return 2 * (x - min_value) / (max_value - min_value) - 1
 
 
 def sigmoid(x: NDArray) -> NDArray:
@@ -66,6 +66,6 @@ def sigmoid_derivative(x: NDArray | float) -> NDArray | float:
 def sigmoid_normalization(x: NDArray | float) -> NDArray | float:
     if isinstance(x, float) or isinstance(x, int):
         return x / abs(x)
-    max_value = max(x)
-    min_value = min(x)
+    max_value = np.max(x)
+    min_value = np.min(x)
     return (x - min_value) / (max_value - min_value)